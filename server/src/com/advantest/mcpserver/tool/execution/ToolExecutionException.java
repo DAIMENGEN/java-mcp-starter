bb874d602df32f0132f2b6b703a999bff5941524
mengen.dai <mengen.dai@advantest.com>
2025-08-06 13:27:44 +0800

    refactor(server): 为 ToolExecutionException 添加序列化版本标识
    为 ToolExecutionException 类添加了 serialVersionUID 字段，以确保类的版本控制和序列化兼容性。这有助于在未来进行类结构更改时，保持向后兼容性。
    
    - 在 ToolExecutionException 类中添加 serialVersionUID 字段
    - 设置初始版本号为20250805221099L

--- server/src/com/advantest/mcpserver/tool/execution/ToolExecutionException.java
+++ server/src/com/advantest/mcpserver/tool/execution/ToolExecutionException.java
@@ -2,11 +2,15 @@ package com.advantest.mcpserver.tool.execution;
 
 import com.advantest.mcpserver.tool.definition.ToolDefinition;
 
+import java.io.Serial;
+
 /**
  * Create on 2025/08/05
  * Author: mengen.dai@advantest.com
  */
 public class ToolExecutionException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 20250805221099L;
     private final ToolDefinition toolDefinition;
 
     public ToolExecutionException(ToolDefinition toolDefinition, Throwable cause) {
